0b56bc926f365a1ba35901d3d460f19801059fe5
alaqsaka <aqsaabidin15@gmail.com>
2021-11-18 21:08:41 +0700

    adding assets and creating header

---
+++ src/components/Header.svelte
@@ -0,0 +1,31 @@
+<header class="xs-header header-transparent">
+    <div class="container">
+        <nav class="xs-menus">
+            <div class="nav-header">
+                <div class="nav-toggle"></div>
+                    <a href="index.html" class="nav-logo">
+                        <img src="assets/images/logo.png" alt="">
+                    </a>
+            </div>
+            <div class="nav-menus-wrapper row">
+                <div class="xs-logo-wrapper col-lg-2 xs-padding-0">
+                    <div class="nav-brand" href="index.html">
+                        <img src="assets/images/logo.png" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <ul class="nav-menu">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="xs-navs-button d-flex-center-end col-lg-3">
+                    <a href="#popularcause" class="btn btn-primary">
+                        <span class="badge"><i class="fa fa-heart"></i></span> Donate Now
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </div>    
+</header>
