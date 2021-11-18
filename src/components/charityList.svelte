d1d9c5d179df7c61860028995c3753dab6ff20e1
alaqsaka <aqsaabidin15@gmail.com>
2021-11-18 20:02:38 +0700

    chore: creating componentes

---
+++ src/components/charityList.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let charities;
+</script>
+
+<div>
+    <h2>Charity List</h2>
+    {#if charities !== undefined}
+    <pre>{charities}</pre>
+    <ul>
+        {#each charities as charity}
+        <li>{charity.title} - {charity.category}</li>
+        {/each}
+    </ul>
+    {:else} 
+        <p>Data belum tersedia</p>
+    {/if}
+</div>
---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: "First Charity Project",
+      category: "Money",
+      thumbnail:
+        "https://images.unsplash.com/photo-1608686207856-001b95cf60ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=627&q=80",
+      pledged: 0,
+      target: 100000,
+      date_end: +new Date("10 August 2021"),
+      profile_photo:
+        "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80",
+      profile_name: "Viroide Bueno",
+      no_pledges: 0,
+    },
+  ],
+};
