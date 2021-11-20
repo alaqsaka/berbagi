47915978f3362120a36aa4284f85f6c4d5ead04a
alaqsaka <aqsaabidin15@gmail.com>
2021-11-20 20:52:59 +0700

    chore: editing welcome page

--- src/components/Welcome.svelte
+++ src/components/Welcome.svelte
@@ -3,15 +3,58 @@
         <div class="xs-welcome-content" style="background-image: url(assets/images/slide1.png);">
             <div class="container">
                 <div class="xs-welcome-wraper color-white">
-                    <h2>Hunger is stalking the globe</h2>
-                    <p>Hundreds of thousands of children experiencing or witnessing assault <br> and other
-                        gender-based violence.</p>
+                    <h2>Kelaparan masih menjadi permasalahan di seluruh penjuru dunia</h2>
+                    <p>Masih banyak keluarga yang kesulitan mencari pemasukan <br> sehingga
+                        kebutuhan sehari-hari tidak dapat terpenuhi.</p>
                     <a href="#popularcause" class="btn btn-outline-primary">
-                        View Causes
+                        Lihat Penyebab
                     </a>
                 </div><!-- .xs-welcome-wraper END -->
             </div><!-- .container end -->
             <div class="xs-black-overlay"></div>
         </div><!-- .xs-welcome-content END -->       
+    </div>
+</section>
+
+<section class="xs-section-padding">
+    <div class="container">
+        <div class="xs-heading xs-mb-70 text-center">
+            <h2 class="xs-mb-0 xs-title">
+                Kami telah menyelesaikan <span>100,000 projek kemanusiaan</span> untuk 10 juta
+                orang dari seluruh penjuru dunia
+            </h2>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-3">
+                <div class="xs-service-promo">
+                    <span class="icon-water"></span>
+                    <h5>Menyediakan air jernih <br>untuk orang tidak mampu</h5>
+                    <p>663 juta orang minum air kotor. Pelajari bagaimana akses ke air bersih dapat meningkatkan kesehatan,
+                        meningkatkan perekonomian lokal.</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="xs-service-promo">
+                    <span class="icon-groceries"></span>
+                    <h5>Membagikan makanan sehat<br> untuk orang yang tidak mampu</h5>
+                    <p>Pada tahun 2020, antara 720 sampai 811 juta orang mengalami kelaparan. Disebabkan pandemi covid-19, tingkat jumlah orang yang mengalami kelaparan meningkat 1.5 percent.</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="xs-service-promo">
+                    <span class="icon-heartbeat"></span>
+                    <h5>Fasilitas kesehatan untuk masyarakat di beberapa negara</h5>
+                    <p>Hampir 2 miliar orang tidak memiliki akses ke obat-obatan dasar, menyebabkan kaskade
+                        dari kesengsaraan dan penderitaan yang dapat dicegah, WHO (World Health Organization).</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="xs-service-promo">
+                    <span class="icon-open-book"></span>
+                    <h5>Edukasi bagi anak yang belum memiliki kesempatan belajar</h5>
+                    <p>Dua pertiga anak usia sekolah di dunia – atau 1,3 miliar anak berusia 3 hingga 17 tahun – tidak memiliki koneksi internet di rumah mereka, menurut laporan bersama baru dari UNICEF dan International Telecommunication Union (ITU).</p>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
