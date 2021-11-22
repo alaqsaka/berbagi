220297828f98847bff7d8072f492038e62fa316a
alaqsaka <aqsaabidin15@gmail.com>
2021-11-23 00:08:48 +0700

    chore: adding footer

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -3,6 +3,7 @@
 	import Welcome from "../components/Welcome.svelte"
 	import CharityList from "../components/charityList.svelte";
 	import Promo from "../components/Promo.svelte";
+	import Footer from "../components/Footer.svelte";
 </script>
 
 
@@ -15,3 +16,4 @@
 <Welcome />
 <CharityList />
 <Promo />
+<Footer />
